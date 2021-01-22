a470454b07cd071782b29526bbd322ff56691450
mohammedimran010 <mohammed.imran010@gmail.com>
2021-01-22 17:31:01 +0000

    Updates

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,8 @@ import Home from "./components/Home";
 import Upload from "./components/Upload";
 
 const navLinks = [
-    { text: 'Home', path: '/', component: Home},
-    { text: 'Upload', path: '/upload', component: Upload},
+  { text: "Home", path: "/", component: Home },
+  { text: "Upload", path: "/upload", component: Upload },
 ];
 
 export default navLinks;
-
